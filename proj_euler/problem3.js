896f28ff85464d40ec79ef133c063b26e4c005da
Tommy Rodriguez <trodriguez91@icloud.com>
2015-01-16 14:28:01 -0700

    Use fermat's factorization method

--- proj_euler/problem3.js
+++ proj_euler/problem3.js
@@ -1,35 +1,5 @@
 /* Find the largest prime factor of 600851475143
-  * if statement to continue to factor until modulo not equal to 0
-  * if % 2 != 0 then 
-*/
-// load('biginteger.js');
-// n = BigInteger(1000);
-// n = 2890;
-// array = [];
-// prime = [];
-// factor = [];
-// answer = [];
-// for (var i = 0; i <= n; i++) {
-//   array.push(i);
-// }
-
-// Create prime array from number n
-// for (var i = 1; i < array.length - 1; i++) {
-//   if (array[i] % 2 != 0 && array[i] % 3 != 0) {
-//     prime.push(array[i]);
-//   }
-// }
-
-// Create factor array from prime array
-// for (var j = prime.length-1; j > 0; j--) {
-//   if (n % prime[j] == 0) {
-//     factor.push(prime[j]);
-//   }
-// }
-// debug(factor);
-
-/* Approach 2
-  Fermat's
+  Fermat's factorization method
   A^2 - N = B^2
   - first try square root and round up
   - if difference is not a perfect square increment up
@@ -42,18 +12,31 @@
 function isInt(n) {
   return n % 1 === 0;
 }
-N = 5959;
-A_0 = Math.sqrt(N);
-A = Math.ceil(A_0);
-B = Math.sqrt((Math.pow(A, 2) - N));
-while (!isInt(B)) {
-  var n = 0;
-  n++;
-  A++;
+
+N = 600851475143;
+
+function factor(N) {
+  A_0 = Math.sqrt(N);
+  A = Math.ceil(A_0);
   B = Math.sqrt((Math.pow(A, 2) - N));
+  while (!isInt(B)) {
+    A++;
+    B = Math.sqrt((Math.pow(A, 2) - N));
   }
 f1 = A - B;
 f2 = A + B;
-debug('The factors are ' + f1 + ' and ' + f2 + '.');
+debug('The factors of ' + N + ' are ' + f1 + ' and ' + f2 + '.');
+}
+factor(N);
+X = f1
+Y = f2
+factor(X);
+M = f1
+N = f2
+factor(Y);
+R = f1
+S = f2
 
-
+// Find greatest prime factor
+answer = Math.max(M, N, R, S);
+print('The greatest prime factor is ' + answer)

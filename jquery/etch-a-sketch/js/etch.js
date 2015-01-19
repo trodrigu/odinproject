b1c641c74229df75ec37b76cc6d1e9207ef9c7f2
Tommy Rodriguez <trodriguez91@icloud.com>
2015-01-18 20:19:26 -0700

    Add reset button, basic set up and resizing

--- jquery/etch-a-sketch/js/etch.js
+++ jquery/etch-a-sketch/js/etch.js
@@ -1,13 +1,59 @@
-n = 0;
+/* increments amount of tries */
+h = 0;
+
+/* Includes setup */
+function grid_reset() {
+  var grid_size = window.prompt( "How big would you like the grid?" );
+  if (grid_size > 64) {
+    alert("Too big.  Anything under 64.");
+  }
+  else {
+    k = Math.sqrt(grid_size);
+    n = 0;
+    grid_setup();
+    h++;
+  }
+}
+
+/* Basic setup */
+function grid_setup() {
   $(document).ready(function() {
-      while (n < 4) {
-        clone = $(".grid").clone();
-        clone.appendTo(".wrapper");
-        n++;
-      }
-      while (n > 0) {
-        clone = $(".wrapper").clone();
-        clone.appendTo(".motherwrapper");
-        n--;
-      }
-  });
+    while (n < k) {
+      clone = $( ".grid" ).clone();
+      clone.appendTo( ".wrapper" );
+      n++;
+    }
+    while (n > 0) {
+      clone = $( ".wrapper" ).clone();
+      clone.appendTo( ".motherwrapper" );
+      n--;
+    }
+});
+}
+
+/* Cleans up the grid system and leaves one grid */
+function grid_remove() {
+    $( ".wrapper" ).remove();
+    $( ".motherwrapper" ).append( "<div class='wrapper'><div class='grid'></div></div>" );
+}
+
+/* Action for button push */
+function reset() {
+  $( ".wrapper" ).find( ".select" ).removeClass( "select" );
+  grid_remove();
+  grid_reset();
+  hoverblack();
+}
+
+function hoverblack() {
+  $(document).ready(function() {
+    $( ".grid" ).hover(
+      function() {
+        $( this ).addClass( "select" );
+      });
+  })
+}
+
+grid_reset();
+hoverblack();
+

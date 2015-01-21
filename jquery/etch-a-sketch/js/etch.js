c983498a2ebac58341b60de2067ac9b455961319
Tommy Rodriguez <trodriguez91@icloud.com>
2015-01-20 20:09:27 -0700

    Add first before clone to fix exponential growth

--- jquery/etch-a-sketch/js/etch.js
+++ jquery/etch-a-sketch/js/etch.js
@@ -1,11 +1,3 @@
-/* increments amount of tries */
-h = 0;
-
-/* Integer test function */
-function isInt(n) {
-  return n % 1 === 0;
-}
-
 /* Includes setup */
 function grid_reset() {
   grid_size = window.prompt( "How big would you like the grid?" );
@@ -13,57 +5,29 @@ function grid_reset() {
     alert("Too big.  Anything under 64.");
   }
   else {
-    k_0 = Math.sqrt(grid_size);
-    console.log(k_0);
-    if (Math.sqrt(!isInt(k_0))) {
-      k = Math.floor(k_0);
-    }
-    else {
-      k = k_0;
-    }
+    k = grid_size;
     n = 0;
     grid_setup();
     console.log(k);
-    h++;
   }
 }
-
 
 /* Basic setup */
 function grid_setup() {
   $(document).ready(function() {
-    if (grid_size > 32) {
-      while (n < k-2) {
-        clone = $( ".grid" ).clone();
-        clone.appendTo( ".wrapper" );
-        n++;
+    while (n < k-1) {
+      clone = $( ".grid" ).first().clone();
+      clone.appendTo( ".wrapper" );
+      n++;
       }
-    }
-    if (grid_size > 16) {
-      while (n < k-1) {
-          clone = $( ".grid" ).clone();
-          clone.appendTo( ".wrapper" );
-          n++;
-        }
-    }
-    else {
-      while (n < k) {
-          clone = $( ".grid" ).clone();
-          clone.appendTo( ".wrapper" );
-          n++;
-        }
-    }
     while (n > 0) {
-      clone = $( ".wrapper" ).clone();
+      clone = $( ".wrapper" ).first().clone();
       clone.appendTo( ".motherwrapper" );
       n--;
     }
 });
 }
 
-function grid_size_mid() {
-  document.writeln( "<div class='wrapper'><div class='grid'></div></div>");
-}
 
 /* Cleans up the grid system and leaves one grid */
 function grid_remove() {

8ff0b0e6d789ea94c6fb07e3ca834c33c69a844d
Tommy Rodriguez <trodriguez91@icloud.com>
2015-02-03 20:31:54 -0700

    translates two words, a word beginning with three consonants, sch as a single phoneme

--- learn_ruby/04_pig_latin/pig_latin.rb
+++ learn_ruby/04_pig_latin/pig_latin.rb
@@ -1,29 +1,47 @@
 # If word starts with a vowel slap an "-ay" on the end
 # If word starts with a consonant, move the consonant to the end and then slap an "-ay" on the end
 def translate(word)
-  s = word
-  # Check for multiple consonants in beginning
-  n = 0
-  c = s[0]
-  front_consonants = ""
-  while c != "a" && c != "e" && c != "i" && c != "o" && c != "u"
-    front_consonants << c
-    # s.slice!(n)
-    n += 1
-    c = s[n]
-  end
-  s << front_consonants
-  puts front_consonants
-  # consonant test
-  # if s[0] != "a" && s[0] != "e" && s[0] != "i" && s[0] != "o" && s[0] != "u"
-  #   r = s.slice(0)
-  #   s << r
-  #   s.slice!(0)
-  # end
+  # Check for multiple words
+  x = word.split
+    s = x[0]
 
-  s << "ay"
-  s = s.downcase
-  puts s
+    # Check for multiple consonants in beginning
+    n = 0
+    c = s[0]
+    front_consonants = ""
+    while c != "a" && c != "e" && c != "i" && c != "o" && c != "u"
+      front_consonants << c
+      n += 1
+      c = s[n]
+    end
+    if n >= 1
+      s.slice!(0..n-1)
+    end
+    s << front_consonants
+    s << "ay"
+    s = s.downcase
+    total = ""
+    total << s
+
+    if x.length > 1
+      t = x[1]
+      # Check for multiple consonants in beginning
+      n = 0
+      c = t[0]
+      front_consonants = ""
+      while c != "a" && c != "e" && c != "i" && c != "o" && c != "u"
+        front_consonants << c
+        n += 1
+        c = t[n]
+      end
+      if n >= 1
+        t.slice!(0..n-1)
+      end
+      t << front_consonants
+      t << "ay"
+      t = t.downcase
+      total << " "
+      total << t
+    end
+  total
 end
-
-translate("cherry")

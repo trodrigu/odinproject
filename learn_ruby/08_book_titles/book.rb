613081ce77f43b7c6c4a393434248d3dc6136685
Tommy Rodriguez <trodriguez91@icloud.com>
2015-02-07 21:08:19 -0700

    capitalize first letter and every word

--- learn_ruby/08_book_titles/book.rb
+++ learn_ruby/08_book_titles/book.rb
@@ -2,11 +2,19 @@ class Book
   attr_reader :title
 
   def title=(str)
-    # separated = str.strip
     separated = str.split
-    # @title = @title.capitalize
     capitalized_words = separated.each { |c| c.capitalize! }
+    # capitalized_words = ""
+    # for i in 0..separated.length-1 do
+    #     capitalized_words << separated[i].capitalize
+    #     puts capitalized_words
+        # puts separated[i]
+    #   if i != "and"
+      # end
+    # end
     @title = capitalized_words.join(" ")
+    # check sort
+
     puts @title
   end
 
